1e09b4b9df93bbd487a5e242a88b7617ff006fa3
aneerProgrammer <nasemalabeer@hotmail.com>
2019-09-03 14:02:52 +0300

    First comment

---
+++ week1day1.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hellow")
+hellow
+>>> if 5>2:
+	print("hellow")
+
+	
+hellow
+>>> if 5>2:
+	print("مرحبا")
+
+	
+مرحبا
+>>> 
